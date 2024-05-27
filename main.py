ef90bc78f4beb0dcba1e4ae50e370fc7329e5533
Mariana Almeida <127327238+Mariana851@users.noreply.github.com>
2024-05-27 08:45:41 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+num1=float(input("Digite o primeiro número: "))
+num2=float(input("Digite o segundo número: "))
+
+soma= num1+num2
+diferenca=num1-num2
+produto=num1*num2
+quociente= num1/num2
+
+print("A soma dos números é: ", soma)
+print(f"A diferença dos números é: {diferenca}")
+print(f"O produto dos números é: {produto}")
+print(f"O quociente dos números é: {quociente}")
